78ed54702826bb4b20b4e05c339c34ce08a6f6ea
ashithFarhan <b00079866@aus.edu>
2020-11-13 16:59:09 +0400

    layout

---
+++ src/Minigame/Terminal.java
@@ -0,0 +1,44 @@
+package Minigame;
+
+import java.util.Scanner;
+
+public class Terminal {
+
+	public static void maintenance() throws InterruptedException {
+		System.out.println("Starting maintenance..."); 
+		Thread.sleep(1500); 
+		System.out.println("Suspending systems...");
+		Thread.sleep(1500);
+		System.out.println("Surveillance systems offline.");
+	}
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Scanner cin = new Scanner(System.in);
+		String files = "README.txt    maintenance.sh    restore.sh";
+
+		while(true)
+		{
+			System.out.print("warden@CamSystem:~$ ");
+			String cmd = cin.nextLine();
+			switch(cmd)
+			{
+				case "whoami": 
+					System.out.println("warden"); break;
+				case "ls": 
+					System.out.println(files); break;
+				case "cat README.txt": 
+					System.out.println("For software updates etc. just run the maintenance script to suspend system.\nPS: Don't forget to restore afterwards"); break;
+				case "./maintenance.sh": 
+					maintenance(); break;
+				case "rm restore.sh": 
+					System.out.println("File deleted."); files = files.split("    ")[0] + "    "+ files.split("    ")[1]; break;
+				case "exit": 
+					System.out.println(""); break;
+				default:
+					System.out.println(cmd+": command not found");
+			}
+
+		}
+	}
+
+}
