0485a850da86cbf079f80cb7de90288dd0ef6584
ashithFarhan <b00079866@aus.edu>
2020-12-05 20:03:19 +0400

    Changed Lock Pick

--- src/Minigame/Pocket.java
+++ src/Minigame/Pocket.java
@@ -3,33 +3,50 @@ package Minigame;
 import Sensor.TCP_Client;
 import Sensor.coord;
 
+
 public class Pocket {
 
 	TCP_Client tcp;
-	Watch w;
+	int progress = 0;
 
 	Pocket(String ip, int port) {
 		this.tcp = new TCP_Client(ip, port);
-		this.w = new Watch(5);
+	
 	}
 
-	void pick()
+	void pick() throws InterruptedException
 	{
-		this.w.t.start();
+		System.out.println("           ___..__\r\n" + 
+				"  __..--\"\"\" ._ __.'\r\n" + 
+				"              \"-..__\r\n" + 
+				"            '\"--..__\";\t\t\t\t\t\t\t\t\t\t\t\t\t\t ,o.          8 8\r\n" + 
+				" ___        '--...__\"\";\t\t\t\t\t\t\t\t\t\t\t\t\t\td   bzzzzzzzza8o8b\r\n" + 
+				"    `-..__ '\"---..._;\"\t\t\t\t\t\t\t\t\t\t\t\t\t\t `o'\r\n" + 
+				"          \"\"\"\"----'     ");
+		System.out.print("\t\t\t____________________________________________________________________________________________________\n\t\t\t");
+
 		while (true)
 		{
-			coord curAcc = this.tcp.accel;
+			coord curGyro = this.tcp.rot;
 			System.out.print("");
-			if ((curAcc.x > 0.5 && curAcc.x < 1.5) || (curAcc.y > 0.5 && curAcc.y < 1.5) || (curAcc.z > 0.5 && curAcc.z < 1.5))
-			{
-				System.out.println("Perfect...\nItem added to inventory!");
-				w.t.interrupt();
+			
+			if (curGyro.x > 2.0 || curGyro.y > 2.0 ||  curGyro.z > 2.0) {
+				System.out.println("\n\t\t\t\t\t\t\t\tYou alerted the guard!");
 				return;
+			}
+			
+			if ((curGyro.x > 0.5 && curGyro.x < 2.0) || (curGyro.y > 0.5 && curGyro.y < 2.0) || (curGyro.z > 0.5 && curGyro.z < 2.0))
+			{
+				progress++;
+				System.out.print("##");
+				Thread.sleep(100);
+				if(progress == 50) {
+					
+					System.out.println("\n\t\t\t\t\t\t\t\tPerfect...\n\t\t\t\t\t\t\tItem added to inventory!");
+					return;
+				}
 			}
 		}
 	}
 
-	public static void main(String[] args) {
-		(new Pocket("192.168.0.159",23232)).pick();
-	}
 }
