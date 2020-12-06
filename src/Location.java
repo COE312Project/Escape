32a8312c50ca908c4e3d81ce77a86766daf85d4c
wtfarooq <b00080205@aus.edu>
2020-12-07 01:20:28 +0400

    Added dummy location class

---
+++ src/Location.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+
+//import GameAssets.GameMap;
+
+public class Location
+{
+    String name;
+
+    ArrayList<Guard> guards;
+    ArrayList<Item> items;
+
+    // Uncomment the following code and the imports to test it
+	/*
+    public static void main(String[] args)
+    {
+        Scanner cin = new Scanner(System.in);
+        GameMap m = new GameMap();
+        while (true)
+        {
+            Location l = new Location();
+            System.out.println("Location name: ");
+            l.name = cin.nextLine();
+            m.setLocation(l.name);
+            m.printMap();
+        }
+    }
+    */
+}
