b7f00b6a584a3cfd09db043a24a2a7dba01e130d
ashithFarhan <b00079866@aus.edu>
2020-12-13 17:10:25 +0400

    warden door warning msg

--- src/Locations/CorridorBDown.java
+++ src/Locations/CorridorBDown.java
@@ -7,7 +7,7 @@ public class CorridorBDown extends Location{
 		super();
 		this.name = "CorridorWest-South";
 		this.desc = "You are at the South side of the West Corridor. ";
-		super.eDesc = "To the East, is the Warden's Office. ";
+		super.eDesc = "To the East, is the Warden's Office. A sign on the door says 'Authorized Personnel Only'!";
 		super.nDesc = "You see the passage extending to the North. ";
 		super.sDesc = "The corridor turns around a corner to the South. ";
 	}
